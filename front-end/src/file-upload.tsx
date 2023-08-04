02743aa2ab4d3963b8af23a38915ef822c97b353
Timothy Wu <TWu@speedlinesolutions.com>
2023-08-04 13:33:05 -0700

    add button step to file upload

--- front-end/src/file-upload.tsx
+++ front-end/src/file-upload.tsx
@@ -1,30 +1,83 @@
-import { Storage } from "aws-amplify";
-import { useState } from "react";
+import { Button, Flex } from "@aws-amplify/ui-react";
+import { StorageManager } from "@aws-amplify/ui-react-storage";
+import {
+  ProcessFileParams,
+  StorageManagerHandle,
+} from "@aws-amplify/ui-react-storage/dist/types/components/StorageManager/types";
+import "@aws-amplify/ui-react/styles.css";
+import { MutableRefObject, useRef, useState } from "react";
 
 const FileUpload = () => {
-    const [file, setFile] = useState<any>(null);
+  const [uploading, setUploading] = useState(false);
+  const [fileCount, setFileCount] = useState(0);
+  const [done, setDone] = useState(false);
+  const [files, setFiles] = useState<any>({});
+  const ref = useRef() as MutableRefObject<StorageManagerHandle>;
 
-    const handleFileChange = (e: any) => {
-        const file = e.target.files[0];
-        setFile(file);
-    }
-
-    const uploadFile = () => {
-        if (!file) return;
-        try {
-            Storage.put(file.name, file);
-            console.log('File uploaded successfully');
-        } catch (error) {
-            console.log('Error uploading file', error);
+  const processFile = async ({ file, key }: ProcessFileParams) => {
+    return new Promise<ProcessFileParams>((resolve, reject) => {
+      const i = setInterval(() => {
+        if (uploading) {
+          resolve({ file, key });
+          clearInterval(i);
         }
-    }
+      }, 30);
+    });
+  };
 
-    return (
-        <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={uploadFile}>Upload File</button>
-        </div>
-    );
-}
+  const onClick = () => {
+    console.log(fileCount);
+    if (fileCount > 0) {
+      setUploading(true);
+    }
+  };
+
+  return (
+    <Flex direction="column">
+      <StorageManager
+        acceptedFileTypes={[",jpeg", ".jpg", ".png"]}
+        accessLevel="public"
+        maxFileCount={1}
+        processFile={processFile}
+        onUploadSuccess={({ key }) => {
+          setFiles((prevFiles: any) => {
+            return {
+              ...prevFiles,
+              [key as any]: {
+                status: "success",
+              },
+            };
+          });
+          setUploading(false);
+          setDone(true);
+        }}
+        displayText={{
+          getRemainingFilesText(count) {
+            setFileCount(count);
+            return `${count} ${count === 1 ? "file" : "files"} uploading`;
+          },
+        }}
+        ref={ref}
+      />
+      {done && Object.keys(files).length > 0 ? (
+        <Button
+          onClick={() => {
+            ref.current.clearFiles();
+            setUploading(false);
+            setDone(false);
+            setFiles({});
+            setFileCount(0);
+          }}
+        >
+          Clear Files
+        </Button>
+      ) : (
+        <Button isLoading={uploading} onClick={onClick} isDisabled={fileCount <= 0}>
+          Upload
+        </Button>
+      )}
+    </Flex>
+  );
+};
 
 export default FileUpload;
